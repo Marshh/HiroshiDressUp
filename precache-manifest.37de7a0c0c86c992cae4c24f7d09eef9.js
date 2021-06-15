0e44935e6d8297085ff91e0e1d362b449b4a534e
Marshall Nguyen <marshahn@uci.edu>
2021-06-15 05:11:54 -0700

    Updates

---
+++ precache-manifest.37de7a0c0c86c992cae4c24f7d09eef9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d4c66a6b636e735c06a2b0631135ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c1d732621f519030db0",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "70594383ca9fc63b478d",
+    "url": "/static/js/2.7068d35b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7068d35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c1d732621f519030db0",
+    "url": "/static/js/main.a9b25a63.chunk.js"
+  },
+  {
+    "revision": "bbb5dbb3db6c3ead2596",
+    "url": "/static/js/runtime-main.a30fc7ff.js"
+  },
+  {
+    "revision": "5eb2e9f83f25e3517b6c7281189c237a",
+    "url": "/static/media/BaseHiroshiBottom1.5eb2e9f8.jpg"
+  },
+  {
+    "revision": "3efeb08969867de4ffbf44603770bb42",
+    "url": "/static/media/BaseHiroshiTorso1.3efeb089.jpg"
+  },
+  {
+    "revision": "ec3351e04889e45e56e2dc7dd28dea12",
+    "url": "/static/media/CowboyHiroshiBottom1.ec3351e0.jpg"
+  },
+  {
+    "revision": "9899035f3f62da82989404e4ac41614d",
+    "url": "/static/media/CowboyHiroshiTorso1.9899035f.jpg"
+  },
+  {
+    "revision": "d1f3d83408a647da16ccf2c0a86d2c24",
+    "url": "/static/media/FunkRollerSkaterHiroshiBottom1.d1f3d834.jpg"
+  },
+  {
+    "revision": "2e2ac6a0cdda0b341dff6c8fff9af0de",
+    "url": "/static/media/FunkRollerSkaterHiroshiTorso1.2e2ac6a0.jpg"
+  },
+  {
+    "revision": "3d4b7f648d5e3ec18c4077d5fe5f0438",
+    "url": "/static/media/KenshiHiroshiBottom1.3d4b7f64.jpg"
+  },
+  {
+    "revision": "bdfab9786f2139f5b12435416a592705",
+    "url": "/static/media/KenshiHiroshiTorso1.bdfab978.jpg"
+  },
+  {
+    "revision": "87b0cc2d215050941b69e3ec99fbf5b7",
+    "url": "/static/media/SalarymanHiroshiBottom1.87b0cc2d.jpg"
+  },
+  {
+    "revision": "03ec9c9ea3e072bee0d782f8a24b5e5b",
+    "url": "/static/media/SalarymanHiroshiTorso1.03ec9c9e.jpg"
+  },
+  {
+    "revision": "2f3d888c6e97b08a28b471927b3e313d",
+    "url": "/static/media/SchoolgirlHiroshiBottom1.2f3d888c.jpg"
+  },
+  {
+    "revision": "5c3bec2719f824f332fd35587f2ac22a",
+    "url": "/static/media/SchoolgirlHiroshiTorso1.5c3bec27.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HiroshiDressUp/precache-manifest.79769675651d65b5c61f4953350fb2d3.js"
+  "/precache-manifest.37de7a0c0c86c992cae4c24f7d09eef9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HiroshiDressUp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
